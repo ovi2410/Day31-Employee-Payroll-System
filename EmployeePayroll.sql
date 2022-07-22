3f829065ea0b4b8d12388763d38e9dfc66c62b0a
ovi2410 <guneshwarifegade@gmail.com>
2022-07-22 16:40:58 +0530

    [ovi]Added:created database Employee Payroll Service

---
+++ EmployeePayroll.sql
@@ -0,0 +1,6 @@
+﻿--UC-1 creating database
+create database EmployeeServices;              --creating database
+select name from sys.databases                 -- Display all databases
+use EmployeeServices                            -- Selecting database to throw queries-- Selecting database to throw queries
+
+
