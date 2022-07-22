0aef07f7d29a3c7558ef19b9fab4251e6c69cae5
ovi2410 <guneshwarifegade@gmail.com>
2022-07-22 16:57:42 +0530

    Added:inserting employee data in table

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -12,3 +12,11 @@ salary float,
 startDate date
 )
 
+--UC-3 Insert values in Table
+Insert into empoyee_payrolls(name,salary,startDate) values
+('shalini',20000,'2021-03-12'),
+('Magesh',25000,'2021-04-18'),                 --inserting record in table
+('Gayathri',10000,'2020-05-13'),
+('Aruna',30000,'2020-08-19');
+
+
